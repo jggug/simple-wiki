61e6093871dad91ffe14fd767042163ca8dabe7d
Tsuyoshi Yamamoto <mottsnite@gmail.com>
2010-10-11 09:51:06 +0900

    first commit

---
+++ SimpleWikiGrailsPlugin.groovy
@@ -0,0 +1,63 @@
+import org.jggug.wiki.GrailsWikiEngineFactoryBean
+import org.codehaus.groovy.grails.commons.ConfigurationHolder
+import org.radeox.engine.context.BaseInitialRenderContext
+import org.springframework.cache.ehcache.EhCacheFactoryBean
+
+class SimpleWikiGrailsPlugin {
+    // the plugin version
+    def version = "0.1"
+    // the version or versions of Grails the plugin is designed for
+    def grailsVersion = "1.3.5 > *"
+    // the other plugins this plugin depends on
+    def dependsOn = [:]
+    // resources that are excluded from plugin packaging
+    def pluginExcludes = [
+            "grails-app/views/error.gsp"
+    ]
+
+    // TODO Fill in these fields
+    def author = "T.Yamamoto"
+    def authorEmail = "tyama _at_ xmldo.jp"
+    def title = "Simple Wiki on radeox"
+    def description = '''\\
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://github.com/jggug/simple-wiki"
+
+    def doWithWebDescriptor = { xml ->
+        // TODO Implement additions to web.xml (optional), this event occurs before 
+    }
+
+    def doWithSpring = {
+        wikiCache(EhCacheFactoryBean) {
+            timeToLive = 300
+        }
+        wikiContext(BaseInitialRenderContext)
+        wikiEngine(GrailsWikiEngineFactoryBean) {
+            cacheService = ref('cacheService')
+            def config = ConfigurationHolder.getConfig()
+            contextPath = config.grails.serverURL
+            context = wikiContext
+        }
+    }
+
+    def doWithDynamicMethods = { ctx ->
+        // TODO Implement registering dynamic methods to classes (optional)
+    }
+
+    def doWithApplicationContext = { applicationContext ->
+        // TODO Implement post initialization spring config (optional)
+    }
+
+    def onChange = { event ->
+        // TODO Implement code that is executed when any artefact that this plugin is
+        // watching is modified and reloaded. The event contains: event.source,
+        // event.application, event.manager, event.ctx, and event.plugin.
+    }
+
+    def onConfigChange = { event ->
+        // TODO Implement code that is executed when the project configuration changes.
+        // The event is the same as for 'onChange'.
+    }
+}
