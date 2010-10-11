eb884d764f7cd7f73bcdbda3b4fa9338129c03e5
Tsuyoshi Yamamoto <mottsnite@gmail.com>
2010-10-11 09:56:53 +0900

    Wiki Tag

---
+++ grails-app/taglib/org/jggug/wiki/SimpleWikiTagLib.groovy
@@ -0,0 +1,48 @@
+package org.jggug.wiki
+
+class SimpleWikiTagLib {
+    static namespace = 'wiki'
+    def wikiEngine
+    def wikiContext
+
+    def show = {attrs, body ->
+        def content = body()
+        def pageId = attrs.pageId?:"0";
+        content=content.replaceAll(/\r\n|\r|\n/,"\n")
+        if(wikiEngine){
+            def text = wikiEngine.render(content.trim(),""+pageId, wikiContext)
+            out<<text
+        }else{
+            println "wikiEngine ERROR"
+            out<<""
+        }
+    }
+
+    /** pankuzu */
+    def topicPath = { attrs, body ->
+        if(attrs.page) {
+            out << "<ul id='crumbs'>"
+            def parentList=parentLink(attrs.page).reverse()
+            parentList.eachWithIndex{ title,idx ->
+                if((idx+1)==parentList.size()) {
+                    out << "<li>${title}</li>"
+                } else {
+                    out << "<li><a href='${createLink(url:'/')}/display/${title}'>"
+                    out << title
+                    out << "</a></li>"
+                }
+            }
+            out << "</ul>"
+        }
+    }
+
+    def parentLink(page) {
+        def ret=[]
+        if(page) {
+            ret << page.title?.encodeAsHTML()
+            parentLink(page.page).each{ ret << it }
+        }
+        return ret
+    }
+
+}
